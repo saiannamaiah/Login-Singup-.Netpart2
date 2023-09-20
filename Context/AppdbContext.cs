f111dea96ffd0cc56806e8783fed173906eb7422
saiannamaiah <annamaiahsai@gmail.com>
2023-09-20 15:55:09 +0530

    c# code

---
+++ Context/AppdbContext.cs
@@ -0,0 +1,20 @@
+﻿using AngularNew.Module;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularNew.Context
+{
+    public class AppdbContext :DbContext 
+    {
+        public AppdbContext(DbContextOptions<AppdbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using AngularNew.Context;
+using AngularNew.Module;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppdbContext _authContext;
+        public UserController(AppdbContext appdbContext)
+        {
+            _authContext = appdbContext;
+            }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]  User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+            var user= await _authContext.Users.FirstOrDefaultAsync(x=> x.Username == userObj.Username && x.Password == userObj.Password);
+            if (user == null)
+                return NotFound(new { Message = "User not found!" });
+            return Ok(new
+            {
+                Message = "Login Success!"
+            });
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisteredUser([FromBody] User userObj)
+        {
+            if(userObj == null)
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+                await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+    }
+}
